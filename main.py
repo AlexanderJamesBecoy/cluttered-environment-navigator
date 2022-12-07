a2b7523b66549970ff2359afea4a2c225264d089
Alexander James Becoy <bec_alexanderjames@outlook.com>
2022-12-07 22:11:49 +0100

    Added mobile_reacher model to represent holonomic robot

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from model import run_albert
+from model import run_mobile_reacher
 import warnings
 
 if __name__ == "__main__":
-    show_warnings = False
-    warning_flag = "default" if show_warnings else "ignore"
-    with warnings.catch_warnings():
-        warnings.filterwarnings(warning_flag)
-        run_albert(render=True)
+    # show_warnings = False
+    # warning_flag = "default" if show_warnings else "ignore"
+    # with warnings.catch_warnings():
+    #     warnings.filterwarnings(warning_flag)
+    #     run_albert(render=True)
+
+    run_mobile_reacher(render=True)
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 from gym_envs_urdf.urdfenvs.robots.albert import AlbertRobot
+from gym_envs_urdf.urdfenvs.robots.generic_urdf import GenericUrdfReacher
 
 def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
     robots = [
@@ -11,15 +12,41 @@ def run_albert(n_steps=1000, render=False, goal=True, obstacles=True):
         dt=0.01, robots=robots, render=render
     )
     action = np.zeros(9)
-    action[0] = 0.2
-    action[1] = 0.0
-    action[5] = -0.1
+    action[0] = 5.0
     ob = env.reset(
-        pos=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.5, 0.0, 1.8, 0.5])
+        pos=np.array([1.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.5, 0.0, 1.8, 0.5])
     )
     print(f"Initial observation : {ob}")
     history = []
     for _ in range(n_steps):
+        ob, _, _, _ = env.step(action)
+        history.append(ob)
+    env.close()
+    return history
+
+def run_mobile_reacher(n_steps=1000, render=False, goal=True, obstacles=True):
+    robots = [
+        GenericUrdfReacher(urdf="mobilePandaWithGripper.urdf", mode="vel"),
+    ]
+    env = gym.make(
+        "urdf-env-v0",
+        dt=0.01, robots=robots, render=render
+    )
+    action = np.zeros(env.n())
+    action[2] = 0.5 
+    # action[2] = 0.5
+    # action[5] = -0.0
+    # action[-1] = 3.5
+    ob = env.reset()
+    print(f"Initial observation : {ob}")
+    history = []
+    for i in range(n_steps):
+        if (int(i / 100)) % 2 == 0:
+            action[-1] = -0.01
+            action[-2] = -0.01
+        else:
+            action[-1] = 0.01
+            action[-2] = 0.01
         ob, _, _, _ = env.step(action)
         history.append(ob)
     env.close()
