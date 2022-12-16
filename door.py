438dc80a2ff67301961d97d86b9f35d5741517e1
Alexander James Becoy <bec_alexanderjames@outlook.com>
2022-12-16 14:46:05 +0100

    Further optimization of the door such that it can rotate/open correctly. Added comments

--- door.py
+++ door.py
@@ -2,32 +2,44 @@ import gym
 import numpy as np
 
 class Door:
+#  This class contains the useful information describing a door. It also contains the goal object `Knob`
+# for which our mobile manipulator tries to reach.
     
     def __init__(self, env, pos, theta, is_flipped=False, is_open=False):
+    # Initialize an object of this class. It requires the pointer to the gym environment `env`, the orientation of the door `theta`.
+    # Booleans describing whether the door is mirrored `is_flipped`, and whether the door is open `is_open` are set to False by default.
+
         self.env = env
         self.pos = pos
         self.theta = theta
-        self.flipped = 1
-        self.open = 0
+        self.flipped = 1    # No mirroring of poses.
+        self.open = 0       # No additive angle.
 
-        if (is_flipped):
+        if (is_flipped):    # If the door is mirrored, -1 will mirror the required poses.
             self.flipped = -1
-        if (is_open):
+        if (is_open):       # If the door is open, an additive angle is added to rotate the door by additional 90 deg.
             self.open = 0.5*np.pi
     
     def draw_door(self):
+    # This function draws the door into gym `env`. No further passing of arguments required.
+
+        # Obtain the dimension of the door and the doorknob, respectively.
         dim_door = np.array([1.0, 0.1, 2.0])
         dim_knob = np.array([0.2, 0.3, 0.2]) # TODO -> into goal object
-
+            
+        # Poses of 2D offset away from the hinge to draw the door. This is due to `env` drawing the shapes centered.
         offset_x = 0.5*np.cos(self.theta+self.open*self.flipped)*self.flipped
         offset_y = 0.5*np.sin(self.theta+self.open*self.flipped)*self.flipped
 
+        # Poses of 2D offset away from the center of the door to draw the doorknob. This is due to `env` drawing the shapes centered.
         offset_x_knob = 0.3*np.cos(self.theta+self.open*self.flipped)*self.flipped
         offset_y_knob = 0.3*np.sin(self.theta+self.open*self.flipped)*self.flipped
 
+        # Absolute 2D poses describing the centered positions of the door and the doorknob, respectively.
         pos_door = [[self.pos[0]+offset_x, self.pos[1]+offset_y, self.theta+self.open*self.flipped]]
         pos_knob = [[self.pos[0]+offset_x+offset_x_knob, self.pos[1]+offset_y+offset_y_knob, self.theta+self.open*self.flipped]]
 
+        # Draw the door and doorknob.
         self.env.add_shapes(shape_type="GEOM_BOX", dim=dim_door, mass=0, poses_2d=pos_door)
         self.env.add_shapes(shape_type="GEOM_BOX",dim=dim_knob, mass=0, poses_2d=pos_knob, place_height=1.0)
 
--- house.py
+++ house.py
@@ -3,37 +3,53 @@ from door import Door
 import numpy as np
 
 class House:
+# This class contains all the useful, but approximated, information that describes a house for which a mobile manipulator
+# will explore. It will contain the goal objects which are the door knobs that is our mobile manipulator will try to reach.
+# Further details of class House:
+# - Walls
+# - Doors
+# - Furniture
+
     def __init__(self, env):
+    # Initialize an object of this class. It only requires the pointer to the gym environment `env`.
+    # Following declarations: 
+    # - `offset` to center the points around the origin (0,0).
+    # - `points` to describe the walls and door hinges (also a vertex describing the end of wall segment).
+    # - `doors` is a list of interactable door objects.
+
         self.env = env
         self.offset = np.array([7.0, 3.5])
         self.points = {
-            'A': np.array([0.0,0.0]),
-            'B': np.array([12.0,0.0]),
-            'C': np.array([0.0,7.0]),
-            'D': np.array([7.0,7.0]),
-            'E': np.array([8.0,7.0]),
-            'F': np.array([11.0,7.0]),
-            'G': np.array([11.0,3.0]),
-            'H': np.array([12.0,3.0]),
-            'I': np.array([8.0,3.0]),
-            'J': np.array([5.0,7.0]),
-            'K': np.array([5.0,3.0]),
-            'L': np.array([6.0,7.0]),
-            'M': np.array([6.0,3.0]),
-            'N': np.array([8.0,4.0]),
-            'O': np.array([0.0,3.0]),
-            'P': np.array([3.0,3.0]),
-            'Q': np.array([3.0,0.0]),
-            'R': np.array([3.0,2.0]),
-            'S': np.array([10.0,3.0]),
-            'T': np.array([10.0,1.0]),
-            'U': np.array([11.0,1.0]),
-            'V': np.array([4.0,3.0]),
+            'A': np.array([0.0,0.0]),   # Wall vertex.
+            'B': np.array([12.0,0.0]),  # Wall vertex.
+            'C': np.array([0.0,7.0]),   # Wall vertex.
+            'D': np.array([7.0,7.0]),   # Wall vertex.
+            'E': np.array([8.0,7.0]),   # Wall vertex / Door hinge to outside.
+            'F': np.array([11.0,7.0]),  # Wall vertex.
+            'G': np.array([11.0,3.0]),  # Wall vertex.
+            'H': np.array([12.0,3.0]),  # Wall vertex.
+            'I': np.array([8.0,3.0]),   # Wall vertex / Door hinge to the kitchen.
+            'J': np.array([5.0,7.0]),   # Wall vertex.
+            'K': np.array([5.0,3.0]),   # Wall vertex.
+            'L': np.array([6.0,7.0]),   # Wall vertex.
+            'M': np.array([6.0,3.0]),   # Wall vertex.
+            'N': np.array([8.0,4.0]),   # Wall vertex.
+            'O': np.array([0.0,3.0]),   # Wall vertex.
+            'P': np.array([3.0,3.0]),   # Wall vertex / Door hinges to the two bedrooms.
+            'Q': np.array([3.0,0.0]),   # Wall vertex.
+            'R': np.array([3.0,2.0]),   # Wall vertex.
+            'S': np.array([10.0,3.0]),  # Wall vertex.
+            'T': np.array([10.0,1.0]),  # Wall vertex.
+            'U': np.array([11.0,1.0]),  # Wall vertex.
+            'V': np.array([4.0,3.0]),   # Wall vertex.
+            'W': np.array([12.0,1.0]),  # Wall vertex / Door hinge to the bathroom.
         }
         self.doors = []
 
     def generate_walls(self):
-        points = np.array([
+    # This function generates and draw the fixed wall segments described in `self.points`.
+
+        points = np.array([ # Generate wall edges
             [self.points['A'], self.points['B']],
             [self.points['A'], self.points['C']],
             [self.points['C'], self.points['D']],
@@ -52,29 +68,34 @@ class House:
         ])
 
         for i in range(points.shape[0]):
+        # Iterate for every wall edge, center the wall vertices with `self.offset`, and draw the wall.
             start_pos = points[i][0] - self.offset
             end_pos = points[i][1] - self.offset
             self.add_wall(start_pos, end_pos)
 
-    def add_wall(self, start_pos, end_pos):
-        vec = end_pos - start_pos
-        avg = (end_pos + start_pos)/2
-        theta = np.arctan2(*vec)
+    def add_wall(self, start_pos, end_pos, wall_thickness=0.3, wall_height=0.5):
+    # This function draws a wall segment into gym `env` from a starting position `start_pos` to a final position `end_pos`.
+    # The default thickness `wall_thickness` and `wall_height` are 30 cm and 50 cm, respectively. They are modifiable.
+
+        vec = end_pos - start_pos       # Obtain a vector from the two points.
+        avg = (end_pos + start_pos)/2   # Obtain the average point between the two points, because
+                                        # gym `env` draws the shape centered.
+        theta = np.arctan2(*vec)        # Obtain the angle of the vector.
     
-        dim = np.array([0.3, np.linalg.norm(vec), 0.5])
-        pos = [[avg[0], avg[1], theta]]
-        self.env.add_shapes(shape_type="GEOM_BOX", dim=dim, mass=0, poses_2d=pos)
+        dim = np.array([wall_thickness, np.linalg.norm(vec), wall_height])  # Obtain the dimension of the wall.
+        pos = [[avg[0], avg[1], theta]]                                     # Describe the position of the wall with average position and angle.
+        self.env.add_shapes(shape_type="GEOM_BOX", dim=dim, mass=0, poses_2d=pos)   # Draw the wall as a geometrical box.
 
     def generate_doors(self):
-        door_bathroom = Door(self.env, pos=self.points['U']-self.offset, is_open=True, theta=0)
+        door_bathroom = Door(self.env, pos=self.points['W']-self.offset, is_open=True, theta=0, is_flipped=True)
         door_bathroom.draw_door()
         door_outdoor = Door(self.env, pos=self.points['E']-self.offset, is_open=True, theta=np.pi)
         door_outdoor.draw_door()
-        door_bedroom1 = Door(self.env, pos=self.points['P']-self.offset, is_open=True, theta=0, is_flipped=True)
+        door_bedroom1 = Door(self.env, pos=self.points['P']-self.offset, is_open=True, theta=0)
         door_bedroom1.draw_door()
-        door_bedroom2 = Door(self.env, pos=self.points['R']-self.offset, is_open=True, theta=0.5*np.pi)
+        door_bedroom2 = Door(self.env, pos=self.points['P']-self.offset, is_open=True, theta=0.5*np.pi, is_flipped=True)
         door_bedroom2.draw_door()
-        door_kitchen = Door(self.env, pos=self.points['I']-self.offset, is_open=True, theta=-0.5*np.pi)
+        door_kitchen = Door(self.env, pos=self.points['I']-self.offset, is_open=True, theta=-0.5*np.pi, is_flipped=True)
         door_kitchen.draw_door()
 
 
