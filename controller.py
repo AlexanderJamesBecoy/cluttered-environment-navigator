c1502e2312cf9dc2b5321ffaacb7a2aa8d4a00ea
Giacomo <giacomomussita@hotmail.comWuser.nameGiacomoMussita>
2022-12-18 23:11:32 +0100

    MPC draft and arm kinematics

--- controller.py
+++ controller.py
@@ -1,5 +1,49 @@
 import numpy as np
+import cvxpy as cp
+from model import Model
 
 class MPController:
-    def __init__(self):
-        self.x = np.diag((1)) # Something
+    def __init__(self, model: Model, weight_tracking: np.ndarray, weight_input: np.ndarray, weight_terminal: np.ndarray,
+    upper_limit_state: float, lower_limit_state: float, upper_limit_input: float, lower_limit_input: float, 
+    dt: float = 0.001, N: int = 5):
+
+        self.model = model # Model of the robot
+        self.dof = len(self.model.dofs) # Number of dof of the robot
+        self.weight_tracking = weight_tracking # Weight matrix of the error in the cost function
+        self.weight_input = weight_input # Weight matrix of the control input in the cost function
+        self.weight_terminal = weight_terminal # # Weight matrix of the terminal error in the cost function
+        self.dt = dt # Time step of each iteration
+        self.N = N # Prediction horizon
+        self.upper_limit_state = upper_limit_state
+        self.lower_limit_state = lower_limit_state
+        self.upper_limit_input = upper_limit_input
+        self.lower_limit_input = lower_limit_input
+
+    def FHOCP(self, state0: np.ndarray, goal: np.ndarray): # Finite Horizon Optimal Control Problem
+
+        self.x = cp.variables((self.dof, self.N + 1)) # Optimization varibles (states) over an horizon N
+        self.u = cp.variables((self.dof, self.N)) # Optimization variables (inputs) over an horizon N
+        self.cost = 0. # Initialization of the cost function
+        self.constraints = [] # Initialization of the constraints
+        self.add_objective_function(state0, goal)
+        self.add_constraints(state0, goal)
+
+        problem = cp.Problem(cp.Minimize(self.cost), self.constraints)
+        problem.solve(solver=cp.OSQP)
+
+        return 
+
+    def add_objective_function(self, state0: np.ndarray, goal: np.ndarray):
+
+        for k in range(self.N): # Iterate over all the steps of the prediction horizon
+            self.cost += cp.quad_form(self.x[:, k] - goal, self.weight_tracking)
+            self.cost += cp.quad_form(self.u[:, k], self.weight_input)
+        self.cost += cp.quad_form(self.x[:, self.N] - goal, self.weight_terminal)
+
+    def add_constraints(self, state0):
+
+        self.constraints += [self.z[:, 0] == state0] # Initial state constraint
+        for k in range(self.N): # Iterate over all the steps of the prediction horizon
+            self.constraints += [self.lower_limit_state <= self.x[:, k] <= self.upper_limit_state]
+            self.constraints += [self.lower_limit_input <= self.u[:, k] <= self.upper_limit_input]
+        self.constraints += [self.lower_limit_state <= self.x[:, self.N] <= self.upper_limit_state]
---
+++ kinematics.py
@@ -0,0 +1,113 @@
+import numpy as np
+from numpy import sin, cos
+from scipy.spatial.transform import Rotation
+
+def forward_kinematics_manipulator(q: np.ndarray) -> dict:
+    """
+    Compute the forward kinematics of the arm given the joints position.
+
+    Args:
+        q (np.ndarray): position of the joints of the manipulator
+
+    Returns:
+        dict: position and orientation of the end-point as a dictionary.
+            `pos`: np.ndarray containing (x, y, z)
+            `orientation`: np.ndarray containing the orientation in quaternions (qx, qy, qz, qw)
+    """
+
+    # d1 =
+    # d3 = 
+    # d5 = 
+    # d7 = 
+    # a3 =
+    # a4 =
+    # a6 =
+    q1, q2, q3, q4, q5, q6, q7 = q[0], q[1], q[2], q[3], q[4], q[5], q[6]
+
+    r11 = cos(q7) * (sin(q6) * (sin(q4) * (sin(q1) * sin(q3) - cos(q1) * cos(q2) *\
+        cos(q3)) - cos(q1) * cos(q4) * sin(q2)) - cos(q6) * (cos(q5) * (cos(q4) * (sin(q1) *\
+        sin(q3) - cos(q1) * cos(q2) * cos(q3)) + cos(q1) * sin(q2) * sin(q4)) + sin(q5) *\
+        (cos(q3) * sin(q1) + cos(q1) * cos(q2) * sin(q3)))) - sin(q7) * (sin(q5) * (cos(q4) *\
+        (sin(q1) * sin(q3) - cos(q1) * cos(q2) * cos(q3)) + cos(q1) * sin(q2) * sin(q4)) -\
+        cos(q5) * (cos(q3) * sin(q1) + cos(q1) * cos(q2) * sin(q3)))
+
+    r12 = - sin(q7) * (sin(q6) * (sin(q4) * (sin(q1) * sin(q3) - cos(q1) * cos(q2) *\
+        cos(q3)) - cos(q1) * cos(q4) * sin(q2)) - cos(q6) * (cos(q5) * (cos(q4) * (sin(q1) *\
+        sin(q3) - cos(q1) * cos(q2) * cos(q3)) + cos(q1) * sin(q2) * sin(q4)) + sin(q5) *\
+        (cos(q3) * sin(q1) + cos(q1) * cos(q2) * sin(q3)))) - cos(q7) * (sin(q5) * (cos(q4) *\
+        (sin(q1) * sin(q3) - cos(q1) * cos(q2) * cos(q3)) + cos(q1) * sin(q2) * sin(q4)) -\
+        cos(q5) * (cos(q3) * sin(q1) + cos(q1) * cos(q2) * sin(q3)))
+
+    r13 = - cos(q6) * (sin(q4) * (sin(q1) * sin(q3) - cos(q1) * cos(q2) * cos(q3)) -\
+        cos(q1) * cos(q4) * sin(q2)) - sin(q6) * (cos(q5) * (cos(q4) * (sin(q1) * sin(q3) -\
+        cos(q1) * cos(q2) * cos(q3)) + cos(q1) * sin(q2) * sin(q4)) + sin(q5) * (cos(q3) *\
+        sin(q1) + cos(q1) * cos(q2) * sin(q3)))
+
+    r21 = sin(q7) * (sin(q5) * (cos(q4) * (cos(q1) * sin(q3) + cos(q2) * cos(q3) *\
+        sin(q1)) - sin(q1) * sin(q2) * sin(q4)) - cos(q5) * (cos(q1) * cos(q3) - cos(q2) *\
+        sin(q1) * sin(q3))) - cos(q7) * (sin(q6) * (sin(q4) * (cos(q1) * sin(q3) + cos(q2) *\
+        cos(q3) * sin(q1)) + cos(q4) * sin(q1) * sin(q2)) - cos(q6) * (cos(q5) * (cos(q4) *\
+        (cos(q1) * sin(q3) + cos(q2) * cos(q3) * sin(q1)) - sin(q1) * sin(q2) * sin(q4)) +\
+        sin(q5) * (cos(q1) * cos(q3) - cos(q2) * sin(q1) * sin(q3))))
+
+    r22 = sin(q7) * (sin(q6) * (sin(q4) * (cos(q1) * sin(q3) + cos(q2) * cos(q3) *\
+        sin(q1)) + cos(q4) * sin(q1) * sin(q2)) - cos(q6) * (cos(q5) * (cos(q4) * (cos(q1) *\
+        sin(q3) + cos(q2) * cos(q3) * sin(q1)) - sin(q1) * sin(q2) * sin(q4)) + sin(q5) *\
+        (cos(q1) * cos(q3) - cos(q2) * sin(q1) * sin(q3)))) + cos(q7) * (sin(q5) * (cos(q4) *\
+        (cos(q1) * sin(q3) + cos(q2) * cos(q3) * sin(q1)) - sin(q1) * sin(q2) * sin(q4)) -\
+        cos(q5) * (cos(q1) * cos(q3) - cos(q2) * sin(q1) * sin(q3)))
+
+    r23 = cos(q6) * (sin(q4) * (cos(q1) * sin(q3) + cos(q2) * cos(q3) * sin(q1)) +\
+        cos(q4) * sin(q1) * sin(q2)) + sin(q6) * (cos(q5) * (cos(q4) * (cos(q1) * sin(q3) +\
+        cos(q2) * cos(q3) * sin(q1)) - sin(q1) * sin(q2) * sin(q4)) + sin(q5) * (cos(q1) *\
+        cos(q3) - cos(q2) * sin(q1) * sin(q3)))
+
+    r31 = sin(q7) * (sin(q5) * (cos(q2) * sin(q4) + cos(q3) * cos(q4) * sin(q2)) +\
+        cos(q5) * sin(q2) * sin(q3)) + cos(q7) * (cos(q6) * (cos(q5) * (cos(q2) * sin(q4) +\
+        cos(q3) * cos(q4) * sin(q2)) - sin(q2) * sin(q3) * sin(q5)) + sin(q6) * (cos(q2) *\
+        cos(q4) - cos(q3) * sin(q2) * sin(q4)))
+
+    r32 = cos(q7) * (sin(q5) * (cos(q2) * sin(q4) + cos(q3) * cos(q4) * sin(q2)) +\
+        cos(q5) * sin(q2) * sin(q3)) - sin(q7) * (cos(q6) * (cos(q5) * (cos(q2) * sin(q4) +\
+        cos(q3) * cos(q4) * sin(q2)) - sin(q2) * sin(q3) * sin(q5)) + sin(q6) * (cos(q2) *\
+        cos(q4) - cos(q3) * sin(q2) * sin(q4)))
+
+    r33 = sin(q6) * (cos(q5) * (cos(q2) * sin(q4) + cos(q3) * cos(q4) * sin(q2)) -\
+        sin(q2) * sin(q3) * sin(q5)) - cos(q6) * (cos(q2) * cos(q4) - cos(q3) * sin(q2) * sin(q4))
+
+    x = d5 * (sin(q4) * (sin(q1) * sin(q3) - cos(q1) * cos(q2) * cos(q3)) - cos(q1) *\
+        cos(q4) * sin(q2)) - d7 * (cos(q6) * (sin(q4) * (sin(q1) * sin(q3) - cos(q1) * cos(q2) *\
+        cos(q3)) - cos(q1) * cos(q4) * sin(q2)) + sin(q6) * (cos(q5) * (cos(q4) * (sin(q1) *\
+        sin(q3) - cos(q1) * cos(q2) * cos(q3)) + cos(q1) * sin(q2) * sin(q4)) + sin(q5) *\
+        (cos(q3) * sin(q1) + cos(q1) * cos(q2) * sin(q3)))) - d3 * cos(q1) * sin(q2) - a3 *\
+        sin(q1) * sin(q3) + a6 * sin(q6) * (sin(q4) * (sin(q1) * sin(q3) - cos(q1) * cos(q2) *\
+        cos(q3)) - cos(q1) * cos(q4) * sin(q2)) - a6 * cos(q6) * (cos(q5) * (cos(q4) * (sin(q1) *\
+        sin(q3) - cos(q1) * cos(q2) * cos(q3)) + cos(q1) * sin(q2) * sin(q4)) + sin(q5) *\
+        (cos(q3) * sin(q1) + cos(q1) * cos(q2) * sin(q3))) + a4 * cos(q4) * (sin(q1) * sin(q3) -\
+        cos(q1) * cos(q2) * cos(q3)) + a3 * cos(q1) * cos(q2) * cos(q3) + a4 * cos(q1) * sin(q2) * sin(q4)
+
+    y = d7 * (cos(q6) * (sin(q4) * (cos(q1) * sin(q3) + cos(q2) * cos(q3) * sin(q1)) +\
+        cos(q4) * sin(q1) * sin(q2)) + sin(q6) * (cos(q5) * (cos(q4) * (cos(q1) * sin(q3) +\
+        cos(q2) * cos(q3) * sin(q1)) - sin(q1) * sin(q2) * sin(q4)) + sin(q5) * (cos(q1) *\
+        cos(q3) - cos(q2) * sin(q1) * sin(q3)))) - d5 * (sin(q4) * (cos(q1) * sin(q3) + cos(q2) *\
+        cos(q3) * sin(q1)) + cos(q4) * sin(q1) * sin(q2)) + a3 * cos(q1) * sin(q3) - d3 * sin(q1) *\
+        sin(q2) - a6 * sin(q6) * (sin(q4) * (cos(q1) * sin(q3) + cos(q2) * cos(q3) * sin(q1)) +\
+        cos(q4) * sin(q1) * sin(q2)) + a6 * cos(q6) * (cos(q5) * (cos(q4) * (cos(q1) * sin(q3) +\
+        cos(q2) * cos(q3) * sin(q1)) - sin(q1) * sin(q2) * sin(q4)) + sin(q5) * (cos(q1) *\
+        cos(q3) - cos(q2) * sin(q1) * sin(q3))) - a4 * cos(q4) * (cos(q1) * sin(q3) + cos(q2) *\
+        cos(q3) * sin(q1)) + a3 * cos(q2) * cos(q3) * sin(q1) + a4 * sin(q1) * sin(q2) * sin(q4)
+
+    z = d1 + d7 * (sin(q6) * (cos(q5) * (cos(q2) * sin(q4) + cos(q3) * cos(q4) *\
+        sin(q2)) - sin(q2) * sin(q3) * sin(q5)) - cos(q6) * (cos(q2) * cos(q4) - cos(q3) *\
+        sin(q2) * sin(q4))) + d5 * (cos(q2) * cos(q4) - cos(q3) * sin(q2) * sin(q4)) + d3 *\
+        cos(q2) + a6 * sin(q6) * (cos(q2) * cos(q4) - cos(q3) * sin(q2) * sin(q4)) + a3 * cos(q3) *\
+        sin(q2) - a4 * cos(q2) * sin(q4) + a6 * cos(q6) * (cos(q5) * (cos(q2) * sin(q4) + cos(q3) *\
+        cos(q4) * sin(q2)) - sin(q2) * sin(q3) * sin(q5)) - a4 * cos(q3) * cos(q4) * sin(q2)
+
+    rotational_matrix = [[r11, r12, r13], [r21, r22, r23], [r31, r32, r33]]
+    r = Rotation.from_matrix(rotational_matrix)
+    r = r.as_quat()
+
+    pose = {'pos': np.array([x, y, z]), 'orientation': r}
+
+    return pose
