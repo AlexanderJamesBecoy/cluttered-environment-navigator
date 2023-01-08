c2bd08bf3fa167c3a990fe7b0446917c9e4ea35e
Alexander James Becoy <bec_alexanderjames@outlook.com>
2023-01-08 19:17:15 +0100

    Moved the classes Door and Knob into house.py to reduce redundancy of files

--- house.py
+++ house.py
@@ -1,5 +1,4 @@
 import gym
-from door import Door
 import pybullet as p
 import numpy as np
 from MotionPlanningEnv.urdfObstacle import UrdfObstacle
@@ -343,3 +342,89 @@ class House:
         self.Obstacles.doors = np.array(self.Obstacles.doors)
         self.Obstacles.knobs = np.array(self.Obstacles.knobs)
 
+class Door:
+    """
+    This class contains the useful information describing a door. It also contains the goal object `Knob`
+    for which our mobile manipulator tries to reach.
+    """
+    
+    def __init__(self, env, pos, theta, is_flipped=False, is_open=False, scale=1.0):
+        """
+        Initialize an object of this class. It requires the pointer to the gym environment `env`, the orientation of the door `theta`.
+        Booleans describing whether the door is mirrored `is_flipped`, and whether the door is open `is_open` are set to False by default.
+        """
+        
+        self.env = env
+        self.pos = pos
+        self.theta = theta
+        self.scale = scale
+        self.flipped = 1    # No mirroring of poses.
+        self.open = 0       # No additive angle.
+        self.dim_door = np.array([1.0*self.scale, 0.1, 2.0])
+        self.dim_knob = np.array([0.2*self.scale, 0.3, 0.2]) # TODO -> into goal object
+        self.pos_door = []
+        self.pos_knob = []
+
+        if (is_flipped):    # If the door is mirrored, -1 will mirror the required poses.
+            self.flipped = -1
+        if (is_open):       # If the door is open, an additive angle is added to rotate the door by additional 90 deg.
+            self.open = 0.5*np.pi
+
+        knobs = []  # List of door knob objects.
+    
+    def draw_door(self):
+        """
+        Draw a door into gym `env`. No further passing of arguments required.
+        """
+
+        offset_x = 0.5*self.scale*np.cos(self.theta+self.open*self.flipped)*self.flipped
+        offset_y = 0.5*self.scale*np.sin(self.theta+self.open*self.flipped)*self.flipped
+
+        # Poses of 2D offset away from the center of the door to draw the doorknob. This is due to `env` drawing the shapes centered.
+        offset_x_knob = 0.3*self.scale*np.cos(self.theta+self.open*self.flipped)*self.flipped
+        offset_y_knob = 0.3*self.scale*np.sin(self.theta+self.open*self.flipped)*self.flipped
+        # Absolute 2D poses describing the centered positions of the door and the doorknob, respectively.
+        self.pos_door = [[self.pos[0]+offset_x, self.pos[1]+offset_y, self.theta+self.open*self.flipped]]
+        self.pos_knob = [[self.pos[0]+offset_x+offset_x_knob, self.pos[1]+offset_y+offset_y_knob, self.theta+self.open*self.flipped]]
+
+        # Draw the door and doorknob.
+        self.env.add_shapes(shape_type="GEOM_BOX", dim=self.dim_door, mass=0, poses_2d=self.pos_door)
+        self.env.add_shapes(shape_type="GEOM_BOX",dim=self.dim_knob, mass=0, poses_2d=self.pos_knob, place_height=1.0)
+
+        # Create door knob objects
+        knobs_offset = np.array([0.15*self.scale*np.sin(self.theta+self.open*self.flipped), 0.15*np.cos(self.theta+self.open*self.flipped), 0])
+        pos_knob_1 = np.hstack((np.array(self.pos_knob)[0][0:2], np.array([1.0]))) - knobs_offset
+        pos_knob_2 = np.hstack((np.array(self.pos_knob)[0][0:2], np.array([1.0]))) + knobs_offset
+        knob_1 = Knob(self.env, pos_knob_1)
+        knob_2 = Knob(self.env, pos_knob_2)
+        knob_1.draw_knob()
+        knob_2.draw_knob()
+
+class Knob:
+    """
+    This class simulates a simple door knob. It only acts a static entity and is used as a goal for our mobile manipulator.
+    """
+
+    def __init__(self, env, pos_3d, radius=0.1):
+        """
+        Declare public variables from the given arguments.
+         - `pos_3d` describes the 3d pose of this knob.
+         - `radius` describes the size of this knob; set default at 10 cm wide.
+        """
+        self.env = env
+        self.pos = pos_3d
+        self.radius = radius
+
+    def draw_knob(self): # TODO -> into goal because add_shapes create obstacles
+        """
+        Draw the knob into the Gym environment `self.env`.
+        """
+        dim = np.array([self.radius])
+        pos = [[self.pos[0], self.pos[1], 0]]
+        self.env.add_shapes(shape_type="GEOM_SPHERE", dim=dim, mass=0, poses_2d=pos, place_height=self.pos[2])
+
+    def get_pos(self):
+        """
+        Returns the 3D position of the door knob `self.pos`.
+        """
+        return self.pos
