3e3cd5836033f593f6a95700aa6413ac8c6d219d
Alexander James Becoy <bec_alexanderjames@outlook.com>
2023-01-11 16:15:42 +0100

    Merge conflict fix

--- main.py
+++ main.py
@@ -13,25 +13,7 @@ R_SCALE = 1.0 #how much to scale the robot's dimensions for collision check
 R_RADIUS = 0.2
 R_HEIGHT = 0.3
 
-# Matplotlib
-def plot_2d(lines, boxes):
-    # Generate 2D plot of house
-    fig, ax = plt.subplots()
-    for line in lines:
-        x = np.array(line['coord'])[:,0]
-        y = np.array(line['coord'])[:,1]
-        if line['type'] == 'wall':
-            color = 'k'
-        else:
-            color = 'r'
-        ax.plot(x,y, color, linewidth=2)
-    for box in boxes:
-        ax.add_patch(
-            Rectangle((box['x'],box['y']),box['w'],box['h'],
-            facecolor='blue',
-            fill=True,
-        ))
-    plt.show()
+
 
 if __name__ == "__main__":
 
@@ -49,32 +31,23 @@ if __name__ == "__main__":
             dt=0.01, robots=robots, render=True
         )
         house = House(env, robot_dim=robot_dim, scale=R_SCALE)
+        planner = Planner(robot=robots[0], house=house)
+
+        # History
+        history = []
 
         # Generate environment
-        start_pos = robots[0].set_initial_pos(3.0,-2.0)
-        ob = env.reset(pos=start_pos)
-        is_open = {
-            'bathroom':         True,
-            'outdoor':          True,
-            'top_bedroom':      True,
-            'bottom_bedroom':   True,
-            'kitchen':          True,
-        }
+        # start_pos = robots[0].set_initial_pos(3.0,-2.0)
+        start_pos, open_doors = planner.plan_motion()
+        init_joints = robots[0].set_initial_pos(start_pos)
+        ob = env.reset(pos=init_joints)
         house.generate_walls()
-        house.generate_doors(is_open)
+        house.generate_doors(open_doors)
         house.generate_furniture()
-
-        lines, boxes = house.generate_plot_obstacles()
-        plot_2d(lines, boxes)
-
-        # print(f"Length: {len(action)}")
-        # print(f"Initial observation : {ob}")
-        # history = []
-
-        # Target position of the robot  
-        waypoints = np.array([[0, -2], [2, -2], [2, 0], [0, 0], [0, 10], [10, 10], [-10, -10]])
+        planner.plot_plan_2d()
 
         # Follow a path set by waypoints
+        waypoints = planner.generate_waypoints()
         robots[0].follow_path(env=env, house=house, waypoints=waypoints)
 
         env.close()
