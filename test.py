486bcd5c8337887f2bfbe447b482793ef3bf3741
Zhengyang Lu <Z.lu-5@student.tudelft.nl>
2023-01-11 23:11:42 +0100

    Fixed multiple MPC instances problem

--- test.py
+++ test.py
@@ -9,8 +9,6 @@ from house import House
 from planner import Planner
 import warnings
 from MPC import MPController
-import time
-from free_space import FreeSpace
 
 TEST_MODE = True # Boolean to initialize test mode to test the MPC
 R_SCALE = 1.0 #how much to scale the robot's dimensions for collision check
@@ -31,30 +29,11 @@ if __name__ == "__main__":
         robots[0]._urdf.center
         env = gym.make("urdf-env-v0", dt=0.01, robots=robots, render=True)
         house = House(env, robot_dim=robot_dim, scale=R_SCALE, test_mode=TEST_MODE)
-        env = gym.make(
-            "urdf-env-v0",
-            dt=0.01, robots=robots, render=True
-        )
-        house = House(env, robot_dim=robot_dim, scale=R_SCALE)
-
-
-        # Generate environment
-        start_pos = robots[0].set_initial_pos([3.0, -2.0])
-        ob = env.reset(pos=start_pos)
-        is_open = {
-            'bathroom':         True,
-            'outdoor':          True,
-            'top_bedroom':      True,
-            'bottom_bedroom':   True,
-            'kitchen':          True,
-        }
         house.generate_walls()
         # house.generate_doors()
         house.generate_furniture()
         planner = Planner(house=house, test_mode=TEST_MODE)
         no_rooms = planner.plan_motion()
-        house.generate_doors()
-        # house.generate_furniture()
 
         # History
         history = []
@@ -85,30 +64,6 @@ if __name__ == "__main__":
                 action = np.zeros(env.n())
                 for i, j in enumerate(robots[0]._dofs):
                     action[j] = actionMPC[i]
-        # Target position of the robot  
-        waypoints = np.array([[0, -2], [2, -2], [2, 0], [0, 0], [0, 10], [10, 10], [-10, -10]])
-
-        # Follow a path set by waypoints
-        # robots[0].follow_path(env=env, house=house, waypoints=waypoints)
-        MPC = MPController(robots[0])
-        goal = np.array([3, -5, 0, 0, 0, 0, 0])
-        action = np.zeros(env.n())
-        k = 0
-
-
-        while(1):
-            ob, _, _, _ = env.step(action)
-            _, b, A, vertices = house.Obstacles.generateConstraintsCylinder(ob['robot_0']['joint_state']['position'])
-            zero_col = np.zeros((b.size, 1))
-            A = np.hstack((A, zero_col))
-            state0 = ob['robot_0']['joint_state']['position'][robots[0]._dofs]
-            # Cfree = FreeSpace(vertices, state0)
-            # A, b = Cfree.update_free_space(state0)
-
-            actionMPC = MPC.solve_MPC(state0, goal, A, b)
-            action = np.zeros(env.n())
-            for i, j in enumerate(robots[0]._dofs):
-                action[j] = actionMPC[i]
 
                 if (k%50 == 0):
                     house.Obstacles.display()
